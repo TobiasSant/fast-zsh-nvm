77ba9f13b008989e1754bff0f6c332dc1bc66f09
TobiasSAnt <santagadatn@gmail.com>
2024-03-06 11:18:09 -0300

    nvm use package.json versions

--- fast-loading-nvm.zsh
+++ fast-loading-nvm.zsh
@@ -10,7 +10,13 @@ function load_nvm() {
         load-nvmrc() {
             if [[ -f .nvmrc && -r .nvmrc ]]; then
                 nvm use
-            elif [[ $(nvm version) != $(nvm version default)  ]]; then
+            elif [[ -f ./package.json && -r ./package.json ]]; then
+                local available_versions=$(node -pe "const pkg = require('./package.json'); const nodeVersions = pkg.engines && pkg.engines.node ? pkg.engines.node.split(' || ') : null; console.log(nodeVersions ? nodeVersions.join('\n') : 'undefined')" | grep -v 'undefined')
+                local installed_versions=$(nvm list node | grep -o '[0-9]\+\.[0-9]\+\.[0-9]\+' | cut -d '.' -f 1)
+                local highest_version=$(comm -12 <(echo "$available_versions" | sort -V) <(echo "$installed_versions" | sort -V) | tail -n 1)
+                echo "Available versions: $(echo $available_versions | tr '\n' ' ')"
+                nvm use $highest_version
+            elif [[ $(nvm version) != $(nvm version default) ]]; then
                 echo "Reverting to nvm default version"
                 nvm use default
             fi
